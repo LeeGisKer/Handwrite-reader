a9ac2176c39a752b439843fc31cd8a99968f2612
LeeGisKer <ax_gil@hotmail.com>
2025-03-19 03:44:20 -0600

    added clean function

--- main.py
+++ main.py
@@ -8,6 +8,15 @@ import uuid
 app = Flask(__name__, template_folder='templates')
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 
+def cleanfolder(folder, max_files=5):
+    files = [os.path.join(folder, f) for f in os.listdir(folder)
+             if os.path.isfile(os.path.join(folder, f))]
+
+    if len(files) > max_files:
+        files.sort(key=lambda x: os.path.getmtime(x))
+        for i in range(len(files) - max_files):
+            os.remove(files[i])
+
 @app.route('/home', methods=['GET', 'POST'])
 
 def home():
@@ -23,11 +32,13 @@ def home():
         file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(file_path)
         
+        cleanfolder(app.config['UPLOAD_FOLDER'], max_files=5)
+        
         text = ocr_image(file_path)
         
         return render_template('results.html', text=text)
-    else:
-        return render_template('index.html')
+    
+    return render_template('index.html')
     
 def ocr_image(image_path):
     img = Image.open(image_path)
@@ -37,6 +48,8 @@ def ocr_image(image_path):
 @app.route('/about')
 def about():
     return "test handwritting reader"
+
+
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
